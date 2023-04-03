1378ba1011ad2ba84411fce1061e7c4c01264745
Gage <77414756+Royuin@users.noreply.github.com>
2023-04-03 18:22:37 -0500

    added functionality to next arrow to allow you to go next slide image

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Drop Down Menu
+
 const drop = document.querySelector('.drop-down-a');
 const dropDownMenu = document.querySelector('.drop-down-menu');
 
@@ -6,10 +8,38 @@ drop.addEventListener('click', () => {
   dropDownMenu.classList.toggle('show');
 });
 
+// Mobile Hamburger Menu
+
 const hamburger = document.querySelector('.hamburger');
 
 hamburger.addEventListener('click', () => {
   const dropDown = document.querySelector('.hamburger-drop-menu');
   dropDown.classList.toggle('hidden');
   dropDown.classList.toggle('show');
+});
+
+// Image Slide
+
+const arrows = document.querySelectorAll('.arrow');
+const slides = document.querySelectorAll('.slide-img');
+
+arrows.forEach((button) => {
+  const slideArray = Array.from(slides);
+
+  button.addEventListener('click', () => {
+    if (button.classList.contains('next')) {
+      const activeSlide = document.querySelector('.active-slide');
+      const currentIndex = slideArray.indexOf(activeSlide);
+      const newIndex = currentIndex + 1;
+
+      activeSlide.classList.remove('active-slide');
+      activeSlide.classList += ' hide-slide';
+
+      const newActiveSlide = slideArray[newIndex];
+      newActiveSlide.classList.remove('hide-slide');
+      newActiveSlide.classList += ' active-slide';
+    } else if (button.classList.contains('previous')) {
+      -1;
+    }
+  });
 });
